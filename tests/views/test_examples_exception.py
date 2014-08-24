be63564295e6017c613ce3b00498db827fe7e592
Robpol86 <robpol86@gmail.com>
2014-08-24 14:25:46 -0700

    Testing pretty HTTP500 template.

--- tests/views/test_examples_exception.py
+++ tests/views/test_examples_exception.py
@@ -2,6 +2,16 @@ from flask import current_app
 import pytest
 
 
+def test_pretty_page():
+    with current_app.test_client() as c:
+        c.application.config['PROPAGATE_EXCEPTIONS'] = False
+        request = c.get('/examples/exception/')
+        c.application.config['PROPAGATE_EXCEPTIONS'] = True
+
+    assert '500 INTERNAL SERVER ERROR' == request.status
+    assert '<title>PyPI Portal - HTTP 500</title>' in request.data
+
+
 def test_exception():
     with pytest.raises(RuntimeError):
         current_app.test_client().get('/examples/exception/')
